ecc842d2be1d567f6dd4aaaa3957b5d390c48e02
Thumbone1 <Thumbone1@users.noreply.github.com>
2015-05-06 23:53:27 +0100

    Update lgscalc.py

--- lgscalc.py
+++ lgscalc.py
@@ -2,9 +2,10 @@
 # For calculating low gravity solids when using calcium carbonate
 # Written by: Judge Gambill
 
+
 def main():
 	init_lgs_prcnt = float(raw_input("Input initial lgs percent > "))
-	init_lgs_wt = float(raw_input("Input initial lgs by weight > "))
+	init_lgs_wt = float(raw_input("Input initial lgs lb/bbl > "))
 	caco3_wt = float(raw_input("Input calcium carbonate lb/bbl > "))
 	
 	corrected_lgs_prcnt = round(init_lgs_prcnt - (caco3_wt * init_lgs_prcnt / init_lgs_wt), 2)
