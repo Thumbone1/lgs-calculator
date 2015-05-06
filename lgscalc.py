8ae920449bf141ca4d14034ddc4642bc8eac41f8
Thumbone1 <Thumbone1@users.noreply.github.com>
2015-05-06 23:48:45 +0100

    Update lgscalc.py

--- lgscalc.py
+++ lgscalc.py
@@ -3,13 +3,13 @@
 # Written by: Judge Gambill
 
 def main():
-	init_lgs_prcnt = float(raw_input("input initial lgs percent > "))
-	init_lgs_wt = float(raw_input("input initial lgs by weight > "))
-	caco3_wt = float(raw_input("input calcium carbonate lb/bbl > "))
-
-	corrected_lgs_prcnt = init_lgs_prcnt - (caco3_wt * init_lgs_prcnt / init_lgs_wt)
-	corrected_lgs_wt = init_lgs_wt - caco3_wt
-
+	init_lgs_prcnt = float(raw_input("Input initial lgs percent > "))
+	init_lgs_wt = float(raw_input("Input initial lgs by weight > "))
+	caco3_wt = float(raw_input("Input calcium carbonate lb/bbl > "))
+	
+	corrected_lgs_prcnt = round(init_lgs_prcnt - (caco3_wt * init_lgs_prcnt / init_lgs_wt), 2)
+	corrected_lgs_wt = round(init_lgs_wt - caco3_wt, 2)
+	
 	print "corrected lgs percent is %r" % corrected_lgs_prcnt
 	print "corrected lgs lb/bbl is %r" % corrected_lgs_wt
 
